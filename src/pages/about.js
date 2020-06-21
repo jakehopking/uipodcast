5788db91d09d47dd2a729713c7af0a855b0fa66e
Jake Hopking <jake@hopking.io>
2020-06-21 14:14:40 +0100

    use default layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,23 @@
-import React from "react"
-import { graphql } from "gatsby"
-import LayoutDefault from "../layouts/layout-default"
+import React from 'react';
+import {graphql} from 'gatsby';
+import LayoutDefault from '../layouts/layout-default';
+import SectionTitleWings from '../components/sections/SectionTitleWings';
+import SectionContent from '../components/sections/SectionContent';
 
-export default function About({ data }) {
+export default function About({data}) {
   return (
     <LayoutDefault>
-      <h1>About {data.site.siteMetadata.title}</h1>
-      <p>
-        We're the only site running on your computer dedicated to showing the
-        best photos and videos of pandas eating lots of food.
-      </p>
+      <SectionTitleWings tag="h2">
+        About {data.site.siteMetadata.title}
+      </SectionTitleWings>
+      <SectionContent>
+        <p>
+          We're the only site running on your computer dedicated to showing the
+          best photos and videos of pandas eating lots of food.
+        </p>
+      </SectionContent>
     </LayoutDefault>
-  )
+  );
 }
 
 export const query = graphql`
@@ -22,4 +28,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
