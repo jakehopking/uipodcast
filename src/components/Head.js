9abda0ab59e5418713b68e76844f53172e2bfb77
Jake Hopking <jake@hopking.io>
2020-06-25 11:17:00 +0100

    google analytics snippet

--- src/components/Head.js
+++ src/components/Head.js
@@ -52,16 +52,22 @@ const Head = () => {
       <meta name="twitter:image" content={`${url}${socialImage}`} />
       <meta name="twitter:card" content="summary" />
 
-      {`<!-- Global site tag (gtag.js) - Google Analytics -->`}
+      {`<!-- Google Tag Manager -->`}
       <script
         async
         src="https://www.googletagmanager.com/gtag/js?id=UA-100706005-3"
       ></script>
-      {`<script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
-        'UA-100706005-3');
-      </script>`}
+
+      {`<!-- Google Analytics -->`}
+      <script>
+        {`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-100706005-3', 'auto');
+        ga('send', 'pageview');`}
+      </script>
     </Helmet>
   );
 };
